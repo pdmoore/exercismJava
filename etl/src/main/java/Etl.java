5f18fd9d1b5c761b8fce097d9b4a23b656ebd7c7
Paul Moore <pdmoore@gmail.com>
2017-01-03 20:52:37 -0800

    brute solution; ignore swap files

--- etl/src/main/java/Etl.java
+++ etl/src/main/java/Etl.java
@@ -1,8 +1,25 @@
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
 
 public class Etl {
     public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-        return null;
+
+        Set<Integer> keys = old.keySet();
+
+        Map<String, Integer> values = new HashMap<String, Integer>();
+
+        for (Integer key : keys) {
+            List<String> mappedToKey = old.get(key);
+
+            for (String singleLetter : mappedToKey) {
+
+                values.put(singleLetter.toLowerCase(), key);
+            }
+        }
+
+
+        return values;
     }
 }
