09b642e5414a5741772bcbeceec2fa8e58da9482
Paul Moore <pdmoore@gmail.com>
2017-01-04 19:53:54 -0800

    basics to get empty dna string passing

--- nucleotide-count/src/main/java/DNA.java
+++ nucleotide-count/src/main/java/DNA.java
@@ -1,2 +1,14 @@
+import java.util.Map;
+
 public class DNA {
+    public DNA(String dnaString) {
+    }
+
+    public Integer count(char a) {
+        return new Integer(0);
+    }
+
+    public Map<Character,Integer> nucleotideCounts() {
+        return null;
+    }
 }
