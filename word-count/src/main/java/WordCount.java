04e7451d849a5948ca93d6ecf32b7e7c9f5e5150
Paul Moore <pdmoore@gmail.com>
2017-01-08 08:45:46 -0800

    naive impl of interface for phrase

--- word-count/src/main/java/WordCount.java
+++ word-count/src/main/java/WordCount.java
@@ -1,2 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordCount {
+    public Map<String,Integer> phrase(String word) {
+        Map<String, Integer> result = new HashMap<>();
+        result.put(word, 1);
+        return result;
+    }
 }
