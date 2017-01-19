c240517f526b296199f77b67ce3951fc8110b7cc
Paul Moore <pdmoore@gmail.com>
2017-01-18 19:17:05 -0800

    more efficient impl - single pass through sentence, return if all letters are found

--- pangram/src/main/java/Pangrams.java
+++ pangram/src/main/java/Pangrams.java
@@ -1,15 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by pdm on 1/17/17.
  */
 public class Pangrams {
     public static boolean isPangram(String sentence) {
-        sentence = sentence.toLowerCase();
+//        sentence = sentence.toLowerCase();
+//
+//        for (Character c = 'a'; c <= 'z'; c++) {
+//            if (sentence.indexOf(c) == -1) {
+//                return false;
+//            }
+//        }
+//        return true;
 
-        for (Character c = 'a'; c <= 'z'; c++) {
-            if (sentence.indexOf(c) == -1) {
-                return false;
+        char[] chars = sentence.toCharArray();
+        Set<Character> allLetters = new HashSet<Character>();
+        for (int i = 0; i < chars.length; i++) {
+            char thisChar = chars[i];
+            if ((thisChar >= 'a' && thisChar <= 'z') || (thisChar >= 'A' && thisChar <='Z')) {
+                allLetters.add(Character.toLowerCase(thisChar));
+                if (allLetters.size() == 26) return true;
             }
         }
-        return true;
+
+        return false;
     }
 }
