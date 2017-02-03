0f0074cf4fe1f90cece3a5deb0be67020c6a45ea
Paul Moore <pdmoore@gmail.com>
2017-02-03 10:56:02 -0800

    dumb implementation passes no match test

--- anagram/src/main/java/Anagram.java
+++ anagram/src/main/java/Anagram.java
@@ -1,2 +1,12 @@
+import java.util.Collections;
+import java.util.List;
+
 public class Anagram {
+    public Anagram(String baseWord) {
+
+    }
+
+    public List<String> match(List<String> strings) {
+        return Collections.EMPTY_LIST;
+    }
 }
